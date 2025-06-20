988643b659abeb67e918b6d971b51deb13a24637
Michael Zhan <michaelzhan2001@gmail.com>
2025-06-20 19:56:43 -0400

    prettier and eslint config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,47 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+// Extend the Window interface to include 'eel'
+declare global {
+  interface Window {
+    eel: {
+      set_host: (host: string) => void;
+      say_hello_py: (
+        message: string,
+      ) => (callback: (response: string) => void) => void;
+    };
+  }
+}
 
 // Point Eel web socket to the instance
-declare const window: any;
-export const eel = window.eel
-eel.set_host( 'ws://localhost:5169' )
-
-// Test anonymous function when minimized. See https://github.com/samuelhwilliams/Eel/issues/363
-function show_log(msg:string) {
-  console.log(msg)
-}
-window.eel.expose(show_log, 'show_log')
+export const eel = window.eel;
+eel.set_host('ws://localhost:5169');
 
 function App() {
   const [input, setInput] = useState<string>('');
   const [response, setResponse] = useState<string>('');
 
-  return (  
-    <div className="App">
+  return (
+    <div className='App'>
       <input
-        type="text"
+        type='text'
         value={input}
         onChange={(e) => setInput(e.target.value)}
-        placeholder="Type something..."
+        placeholder='Type something...'
       />
-      <button onClick={() => eel.say_hello_py(input)((response: string) => setResponse(response))}>Send to Python</button>
+      <button
+        onClick={() =>
+          eel.say_hello_py(input)((response: string) => setResponse(response))
+        }
+      >
+        Send to Python
+      </button>
 
       <div>
         <h1>Response</h1>
         <p>{response}</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
