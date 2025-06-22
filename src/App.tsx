6b57ae074e433dc8f3bd24033150704f4b55041b
Michael Zhan <michaelzhan2001@gmail.com>
2025-06-21 22:53:07 -0400

    move eel import to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
 import { useState } from 'react';
-
-// Extend the Window interface to include 'eel'
-declare global {
-  interface Window {
-    eel: {
-      set_host: (host: string) => void;
-      make_curl_request: (
-        url: string,
-      ) => (callback: (response: string) => void) => void;
-    };
-  }
-}
-
-// Point Eel web socket to the instance
-export const eel = window.eel;
-eel.set_host('ws://localhost:5169');
+import { eel } from '@/main';
 
 function App() {
   const [input, setInput] = useState<string>('');
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+// Extend the Window interface to include 'eel'
+declare global {
+  interface Window {
+    eel: {
+      set_host: (host: string) => void;
+      make_curl_request: (
+        url: string,
+      ) => (callback: (response: string) => void) => void;
+    };
+  }
+}
+
+// Point Eel web socket to the instance
+export const eel = window.eel;
+eel.set_host('ws://localhost:5169');
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
